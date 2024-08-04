61fa214886dfdcfcc58f0a077e87164f4bc0f32b
erwinmareto <100197413+erwinmareto@users.noreply.github.com>
2024-08-04 19:18:16 +0700

    feat: slicing dashboard chart (#6)

--- src/components/pages/Dashboard/index.tsx
+++ src/components/pages/Dashboard/index.tsx
@@ -1,9 +1,19 @@
-import { CalendarCheck, CalendarClock, CalendarX } from 'lucide-react';
+import { CalendarCheck, CalendarClock, CalendarX, TrendingUp } from 'lucide-react';
 
 import OverviewCard from '@/components/parts/OverviewCard';
+import PaymentChart from '@/components/parts/PaymentChart';
 import SubscriptionTable from '@/components/parts/SubscriptionTable';
 import { columns } from '@/components/parts/SubscriptionTable/columns';
 import { Subscription } from '@/components/parts/SubscriptionTable/types';
+import { Badge } from '@/components/ui/badge';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select';
 
 const Dashboard = () => {
   const data: Subscription[] = [
@@ -165,6 +175,50 @@ const Dashboard = () => {
         <h6 className="font-semibold text-primary-80 text-heading-6">My Subscriptions</h6>
         <div className="bg-primary-0 p-5 mt-4">
           <SubscriptionTable columns={columns} data={data} />
+        </div>
+      </section>
+
+      <section>
+        <h6 className="font-semibold text-primary-80 lg:text-heading-6">Payment History</h6>
+        <div className="bg-primary-0 p-5 mt-4">
+          <div className="flex justify-between items-center mb-4">
+            <div className="flex flex-col gap-2">
+              <p className="font-medium text-primary-80 text-body-xs md:text-body-lg">Time Frame</p>
+              <Select>
+                <SelectTrigger className="bg-muted">
+                  <SelectValue placeholder="This year" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectItem value="apple">2023-2024</SelectItem>
+                    <SelectItem value="banana">20222-2023</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+
+            <div className="flex gap-7">
+              <div>
+                <p className="font-medium text-primary-50 text-body-xs md:text-body-sm">
+                  Spent Last Year
+                </p>
+                <h4 className="font-semibold text-heading-6 md:text-heading-4">Rp 575.000</h4>
+              </div>
+              <div>
+                <p className="font-medium text-primary-50 text-body-xs md:text-body-sm">
+                  Spent This Year
+                </p>
+                <div className="flex justify-center items-center gap-2">
+                  <h4 className="font-semibold text-heading-6 md:text-heading-4">Rp 825.000</h4>
+
+                  <Badge variant="active" className="text-[0.5rem] h-5 border-success-foreground">
+                    <TrendingUp className="w-3 h-3" /> + 20
+                  </Badge>
+                </div>
+              </div>
+            </div>
+          </div>
+          <PaymentChart />
         </div>
       </section>
     </div>
