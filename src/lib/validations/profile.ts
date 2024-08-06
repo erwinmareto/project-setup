2289cb2650abd343964133a45605365812055532
Erwin Mareto Wikas <erwinmaretow@gmail.com>
2024-08-07 00:11:08 +0700

    feat: created profile form validation

---
+++ src/lib/validations/profile.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod';
+
+export const profileSchema = z.object({
+  username: z.string().min(2).max(50),
+  email: z.string().min(2).max(50),
+  location: z.string().min(2).max(50),
+  phoneNumber: z.string().min(10).max(15)
+});
