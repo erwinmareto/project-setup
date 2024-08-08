ee1059949edb286e297dffcba24b68cba412f896
Erwin Mareto Wikas <erwinmaretow@gmail.com>
2024-08-08 20:45:44 +0700

    feat: created validation for step 2 of add form

--- src/lib/validations/add.ts
+++ src/lib/validations/add.ts
@@ -5,3 +5,20 @@ export const step1Schema = z.object({
   appName: z.string().min(2).max(50),
   category: z.string().min(2).max(50)
 });
+
+export const step2Schema = z
+  .object({
+    cycle: z.string().min(2).max(50),
+    paymentStart: z.date(),
+    paymentEnd: z.date(),
+    price: z.string().min(2).max(50),
+    paymentMethod: z.string().min(2).max(50)
+  })
+  .refine((data) => data.paymentStart < data.paymentEnd, {
+    message: 'Payment Start must be before Payment End',
+    path: ['paymentStart']
+  })
+  .refine((data) => data.paymentEnd > data.paymentStart, {
+    message: 'Payment End must be after Payment Start',
+    path: ['paymentEnd']
+  });
