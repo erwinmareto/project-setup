4fc71984fead22db6bc3a61b0f33bae494137d3b
Erwin Mareto Wikas <erwinmaretow@gmail.com>
2024-08-08 20:57:50 +0700

    fix: enabled to pick date beyond current date on the calendars

--- src/components/parts/SubscriptionForms/step2.tsx
+++ src/components/parts/SubscriptionForms/step2.tsx
@@ -104,7 +104,6 @@ const Step2Form = () => {
                         mode="single"
                         selected={field.value}
                         onSelect={field.onChange}
-                        disabled={(date) => date > new Date() || date < new Date('1900-01-01')}
                         initialFocus
                       />
                     </PopoverContent>
@@ -142,7 +141,6 @@ const Step2Form = () => {
                         mode="single"
                         selected={field.value}
                         onSelect={field.onChange}
-                        disabled={(date) => date > new Date() || date < new Date('1900-01-01')}
                         initialFocus
                       />
                     </PopoverContent>
