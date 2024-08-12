f4972e5ba2e08cf42a0ac4edbdfbc2f4e1955da5
Erwin Mareto Wikas <erwinmaretow@gmail.com>
2024-08-12 23:13:26 +0700

    feat: created register form

--- src/lib/validations/auth.ts
+++ src/lib/validations/auth.ts
@@ -8,3 +8,23 @@ export const loginSchema = z.object({
     message: 'Password must be at least 4 characters.'
   })
 });
+
+export const registerSchema = z
+  .object({
+    name: z.string().min(2, {
+      message: 'Name must be at least 2 characters.'
+    }),
+    email: z.string().min(2, {
+      message: 'Invalid email'
+    }),
+    password: z.string().min(6, {
+      message: 'Password must be at least 4 characters.'
+    }),
+    confirmPassword: z.string().min(6, {
+      message: 'Password must be at least 4 characters.'
+    })
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords do not match',
+    path: ['confirmPassword']
+  });
