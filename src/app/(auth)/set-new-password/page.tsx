5a0302e5f0012cc9f3de82fa3656b858a8fd153e
Erwin Mareto Wikas <erwinmaretow@gmail.com>
2024-08-13 01:25:21 +0700

    feat: created set new password form

---
+++ src/app/(auth)/set-new-password/page.tsx
@@ -0,0 +1,5 @@
+import SetNewPassword from '@/components/pages/SetNewPasswordPage';
+
+export default function SetNewPasswordPage() {
+  return <SetNewPassword />;
+}
--- src/lib/validations/auth.ts
+++ src/lib/validations/auth.ts
@@ -34,3 +34,17 @@ export const forgotPasswordSchema = z.object({
     message: 'Invalid email'
   })
 });
+
+export const setNewPasswordSchema = z
+  .object({
+    password: z.string().min(6, {
+      message: 'Password must be at least 6 characters.'
+    }),
+    confirmPassword: z.string().min(6, {
+      message: 'Password must be at least 6 characters.'
+    })
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords do not match',
+    path: ['confirmPassword']
+  });
