f0c07199e258a73849760fc6a695d20de3517f86
Erwin Mareto Wikas <erwinmaretow@gmail.com>
2024-08-13 17:55:56 +0700

    feat: revamped popups to use dropdown menus, adjusted styles, and added links

--- src/components/parts/Navbar/RightContent.tsx
+++ src/components/parts/Navbar/RightContent.tsx
@@ -8,69 +8,87 @@ import Link from 'next/link';
 import NotificationCard from '@/components/parts/Navbar/NotificationCard';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
 import { Separator } from '@/components/ui/separator';
 
 const RightContent = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const handleOpen = () => setIsOpen(!isOpen);
+  const handleIsOpen = () => setIsOpen(!isOpen);
 
   return (
     <div className="flex justify-center items-center gap-5">
-      <Popover open={isOpen} onOpenChange={handleOpen}>
-        <PopoverTrigger>
+      <DropdownMenu open={isOpen} onOpenChange={handleIsOpen}>
+        <DropdownMenuTrigger>
           <Bell />
-        </PopoverTrigger>
+        </DropdownMenuTrigger>
 
-        <PopoverContent className="w-[21rem] flex flex-col gap-5 mt-3 lg:mr-[8rem]">
-          <div className="flex justify-between">
+        <DropdownMenuContent className="w-[21rem] flex flex-col mt-3 lg:mr-[8rem]">
+          <div className="flex justify-between py-3 mx-3">
             <p className="font-medium text-body-md text-primary-70">Notifications</p>
-            <X onClick={handleOpen} className="cursor-pointer" />
+            <X onClick={handleIsOpen} className="cursor-pointer" />
           </div>
-          <div className="flex flex-col gap-2">
+
+          <DropdownMenuItem>
             <NotificationCard />
+          </DropdownMenuItem>
+
+          <DropdownMenuSeparator className="mx-3" />
+
+          <DropdownMenuItem>
             <NotificationCard />
+          </DropdownMenuItem>
+
+          <DropdownMenuSeparator className="mx-3" />
+
+          <DropdownMenuItem>
             <NotificationCard />
-            <Link href="/notification" className="link mt-2 ">
-              See all notifications
-              <ArrowUpRightIcon className="w-4 h-4" />
-            </Link>
-          </div>
-        </PopoverContent>
-      </Popover>
+          </DropdownMenuItem>
+
+          <Link href="/notifications" className="link mt-2 mb-3 ">
+            See all notifications
+            <ArrowUpRightIcon className="w-4 h-4" />
+          </Link>
+        </DropdownMenuContent>
+      </DropdownMenu>
 
       <Separator orientation="vertical" />
 
-      <Popover>
-        <PopoverTrigger>
-          <div className="flex justify-center items-center gap-3">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="ghost"
+            className="flex justify-center items-center gap-3 hover:bg-transparent p-0"
+          >
             <Avatar>
               <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
               <AvatarFallback>CN</AvatarFallback>
             </Avatar>
             <ChevronDown />
-          </div>
-        </PopoverTrigger>
-        <PopoverContent className="w-44 flex flex-col p-1 mr-8">
-          <Button
-            className="justify-start text-body-sm text-primary-80 
-          bg-primary-0 hover:bg-secondary-0 hover:text-secondary-40"
-          >
-            My Profile
           </Button>
-          <Button
-            className="justify-start text-body-sm text-primary-80 
-          bg-primary-0 hover:bg-secondary-0 hover:text-secondary-40"
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-44 flex flex-col p-1 mr-8">
+          <Link href="/profile">
+            <DropdownMenuItem>My Profile</DropdownMenuItem>
+          </Link>
+          <Link href="/general">
+            <DropdownMenuItem>General Settings</DropdownMenuItem>
+          </Link>
+          <DropdownMenuSeparator className="my-1" />
+          <DropdownMenuItem
+            className="bg-destructive text-destructive-foreground 
+          focus:bg-destructive/80 focus:text-destructive-foreground/80"
           >
-            General Settings
-          </Button>
-          <Separator className="my-1" />
-          <Button variant="destructive" className="justify-start hover:bg-destructive-hover">
             Sign out
-          </Button>
-        </PopoverContent>
-      </Popover>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 };
