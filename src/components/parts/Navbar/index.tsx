c0a77764831f1fafc23efda7bffede2d4bb64013
Erwin Mareto Wikas <erwinmaretow@gmail.com>
2024-08-13 20:38:10 +0700

    feat: added conditional class names depending on the pathname on dashboard, my subscription, and settings

--- src/components/parts/Navbar/index.tsx
+++ src/components/parts/Navbar/index.tsx
@@ -1,12 +1,18 @@
+'use client';
+
 import { LayoutDashboard } from 'lucide-react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 import { CircleDollar, WalletWithCards } from '@/assets/icons';
 import RightContent from '@/components/parts/Navbar/RightContent';
 import { Avatar } from '@/components/ui/avatar';
 import { Separator } from '@/components/ui/separator';
+import { cn } from '@/lib/utils';
 
 const Navbar = () => {
+  const pathname = usePathname();
+
   return (
     <header className="bg-primary-0 mb-3">
       <nav className="container flex justify-between py-4">
@@ -15,7 +21,7 @@ const Navbar = () => {
             <CircleDollar />
           </Avatar>
           <div>
-            <p className="text-body-md font-semibold text-primary-70">Remideroo</p>
+            <p className="text-body-md font-semibold text-primary-70">Reminderoo</p>
             <p className="text-body-xs text-primary-45">by Loan Shark&trade;</p>
           </div>
         </div>
@@ -28,15 +34,24 @@ const Navbar = () => {
       <nav className="container flex gap-7 py-4">
         <Link
           href="/dashboard"
-          className="flex justify-center items-center gap-2 font-medium text-body-lg text-secondary-40"
+          className={cn(
+            'flex justify-center items-center gap-2 font-medium text-body-lg',
+            pathname === '/dashboard'
+              ? 'text-secondary-40'
+              : 'text-primary-50 transition-colors hover:text-secondary-40'
+          )}
         >
           <LayoutDashboard />
           Dashboard
         </Link>
         <Link
           href="/my-subscriptions"
-          className="flex justify-center items-center gap-2 font-medium text-body-lg 
-          text-primary-50 transition-colors hover:text-secondary-40"
+          className={cn(
+            'flex justify-center items-center gap-2 font-medium text-body-lg',
+            pathname === '/my-subscriptions'
+              ? 'text-secondary-40'
+              : 'text-primary-50 transition-colors hover:text-secondary-40'
+          )}
         >
           <WalletWithCards />
           My Subscriptions
