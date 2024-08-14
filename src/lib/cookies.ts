1b92970ad8ad4de1e36f32a7548435e829744aa7
Erwin Mareto Wikas <erwinmaretow@gmail.com>
2024-08-14 21:46:15 +0700

    created functions to handle cookies

---
+++ src/lib/cookies.ts
@@ -0,0 +1,31 @@
+import { getCookie as getCookieNext, setCookie } from 'cookies-next';
+import { OptionsType } from 'cookies-next/lib/types';
+
+import { ACCESS_TOKEN_KEY } from './constants';
+
+export const getNextCookieStore = () => {
+  const { cookies } = require('next/headers');
+
+  return cookies();
+};
+
+export const getCookie = (name: string) => {
+  const isServer = typeof window === 'undefined';
+
+  if (isServer) {
+    const cookiesStore = getNextCookieStore();
+
+    return cookiesStore.get(name)?.value;
+  }
+
+  return getCookieNext(name);
+};
+
+export const setAccessToken = (accessToken: string, options?: OptionsType) => {
+  const MAX_AGE_ACCESS_TOKEN = 60 * 60 * 24 * 7; // 1 week
+
+  setCookie(ACCESS_TOKEN_KEY as string, accessToken, {
+    maxAge: MAX_AGE_ACCESS_TOKEN,
+    ...options
+  });
+};
