d01c0440e5aff95ab14ccfc9e26f11c8d94479e7
Erwin Mareto Wikas <erwinmaretow@gmail.com>
2024-08-14 22:04:58 +0700

    feat: auto redirect from / to /dashboard

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: false // Set to true if it's a permanent redirect (301)
+      }
+    ];
+  }
+};
 
 export default nextConfig;
