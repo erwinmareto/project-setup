301df9eb8da3ed3aa8dd623824ad265196ce7e8f
Erwin Mareto Wikas <erwinmaretow@gmail.com>
2024-08-15 22:25:35 +0700

    feat: replaced static data and fixed the UI

--- src/components/parts/Sidebar/SubCard.tsx
+++ src/components/parts/Sidebar/SubCard.tsx
@@ -1,27 +1,40 @@
+import { differenceInDays, format } from 'date-fns';
+
 import { WalletWithCards } from '@/assets/icons';
+import { SubscriptionCategory } from '@/components/parts/SubscriptionTable/types';
 import { Badge } from '@/components/ui/badge';
 
-const SubCard = ({ history }: { history?: boolean }) => {
+export type SubCardProps = {
+  history?: boolean;
+  title: string;
+  category: SubscriptionCategory;
+  paymentDate: string;
+};
+
+const SubCard = ({ title, category, paymentDate, history }: SubCardProps) => {
   return (
-    <div className="flex justify-center items-center gap-10">
-      <div className="flex justify-center items-center gap-3 py-3">
-        <div className="w-11 h-11 flex justify-center items-center rounded-xl bg-red-500">
+    <article className="flex justify-between items-center gap-10">
+      <section className="flex justify-center items-center gap-3 py-3">
+        <div className="w-11 h-11 flex flex-shrink-0 justify-center items-center rounded-xl bg-red-500">
           <WalletWithCards />
         </div>
         <div>
-          <p className="font-semibold text-body-md">Creative Cloud</p>
-          <p className="font-semibold text-body-xs text-primary-50">Work</p>
+          <p className="font-semibold text-body-md">{title}</p>
+          <p className="font-semibold text-body-xs text-primary-50 capitalize">{category}</p>
         </div>
-      </div>
-      <div className="flex flex-col justify-end items-center">
-        <p className="font-semibold text-body-xs text-primary-55">10 July 2024</p>
+      </section>
+
+      <section className="flex flex-col flex-shrink-0 justify-end items-center">
+        <p className="font-semibold text-body-xs text-primary-55">{format(new Date(paymentDate), 'dd MMM yyyy')}</p>
         {history ? (
           <p className="font-semibold text-body-md text-primary-80">-Rp 20.000</p>
         ) : (
-          <Badge className="bg-destructive text-destructive-foreground hover:bg-destructive-hover">7 Days Left</Badge>
+          <Badge className="bg-destructive text-destructive-foreground hover:bg-destructive-hover flex-initial">
+            {differenceInDays(paymentDate, new Date())} Days Left
+          </Badge>
         )}
-      </div>
-    </div>
+      </section>
+    </article>
   );
 };
 
--- src/components/parts/Sidebar/index.tsx
+++ src/components/parts/Sidebar/index.tsx
@@ -1,11 +1,28 @@
+'use client';
+
+import { differenceInDays } from 'date-fns';
 import { ArrowUpRight } from 'lucide-react';
 import Link from 'next/link';
 
+import ReactQuery from '@/components/parts/ReactQuery';
+import { Subscription } from '@/components/parts/SubscriptionTable/types';
 import { Separator } from '@/components/ui/separator';
+import { useAllSubscriptions } from '@/queries/subscriptions';
 
 import SubCard from './SubCard';
 
 const Sidebar = () => {
+  const allSubscripitonsQuery = useAllSubscriptions();
+
+  const getExpiringSubs = (subs: Subscription[]) => {
+    const filteredSubs = subs.filter(
+      (sub: Subscription) =>
+        differenceInDays(sub.nextPayment, new Date()) > 0 && differenceInDays(sub.nextPayment, new Date()) <= 7
+    );
+
+    return filteredSubs;
+  };
+
   return (
     <aside className="flex flex-col gap-6 lg:grid lg:col-span-3">
       <div className="flex flex-col items-center gap-6">
@@ -13,11 +30,18 @@ const Sidebar = () => {
           <h6 className="text-heading-6 font-semibold">Next Payment</h6>
           <div className=" bg-primary-0 rounded-xl w-[21rem]">
             <div className="px-5">
-              <SubCard />
-              <Separator />
-              <SubCard />
-              <Separator />
-              <SubCard />
+              <ReactQuery
+                queryResult={allSubscripitonsQuery}
+                render={(data) => {
+                  const filtered = getExpiringSubs(data);
+                  return filtered.map((sub, index) => (
+                    <>
+                      <SubCard key={sub.id} title={sub.appName} category={sub.category} paymentDate={sub.nextPayment} />
+                      {index < filtered.length - 1 && <Separator />}
+                    </>
+                  ));
+                }}
+              />
             </div>
           </div>
         </article>
@@ -26,11 +50,11 @@ const Sidebar = () => {
           <h6 className="text-heading-6 font-semibold">Payment History</h6>
           <div className="w-[21rem] bg-primary-0 rounded-xl">
             <div className="px-5">
-              <SubCard history />
+              {/* <SubCard history />
               <Separator />
               <SubCard history />
               <Separator />
-              <SubCard history />
+              <SubCard history /> */}
 
               <Separator />
               <Link href="/my-subscriptions?tabs=history" className="link py-3">
