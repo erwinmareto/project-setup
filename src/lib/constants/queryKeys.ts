7a779c1785a326da32c7929209497a68271435de
Erwin Mareto Wikas <erwinmaretow@gmail.com>
2024-08-18 22:04:58 +0700

    feat: connect get profile by id and edit profile to mock API

--- src/lib/constants/queryKeys.ts
+++ src/lib/constants/queryKeys.ts
@@ -1,3 +1,5 @@
 export const ALL_SUBSCRIPTIONS_KEY = 'all-subs';
 
 export const ALL_TRANSACTIONS_KEY = 'all-transaction';
+
+export const PROFILE_BY_ID = 'profile-by-id';
--- src/lib/validations/profile.ts
+++ src/lib/validations/profile.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const profileSchema = z.object({
-  username: z.string().min(2).max(50),
+  name: z.string().min(2).max(50),
   email: z.string().min(2).max(50),
   location: z.string().min(2).max(50),
   phoneNumber: z.string().min(10).max(15)
