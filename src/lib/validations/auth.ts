42a9b2199da7cdb1e10708795c34954a4287e463
Erwin Mareto Wikas <erwinmaretow@gmail.com>
2024-09-04 21:51:02 +0700

    feat: added custom error message on auth validations

--- src/lib/validations/auth.ts
+++ src/lib/validations/auth.ts
@@ -1,26 +1,26 @@
 import { z } from 'zod';
 
 export const loginSchema = z.object({
-  email: z.string().min(2, {
+  email: z.string({ required_error: 'Email is required' }).min(2, {
     message: 'Invalid email'
   }),
-  password: z.string().min(6, {
+  password: z.string({ required_error: 'Password is required' }).min(6, {
     message: 'Password must be at least 6 characters.'
   })
 });
 
 export const registerSchema = z
   .object({
-    name: z.string().min(2, {
+    name: z.string({ required_error: 'Name is required' }).min(2, {
       message: 'Name must be at least 2 characters.'
     }),
-    email: z.string().min(2, {
+    email: z.string({ required_error: 'Email is required' }).min(2, {
       message: 'Invalid email'
     }),
-    password: z.string().min(6, {
+    password: z.string({ required_error: 'Password is required' }).min(6, {
       message: 'Password must be at least 6 characters.'
     }),
-    confirmPassword: z.string().min(6, {
+    confirmPassword: z.string({ required_error: 'Confirm Password is required' }).min(6, {
       message: 'Password must be at least 4 characters.'
     })
   })
@@ -30,17 +30,17 @@ export const registerSchema = z
   });
 
 export const forgotPasswordSchema = z.object({
-  email: z.string().min(2, {
+  email: z.string({ required_error: 'Email is required' }).min(2, {
     message: 'Invalid email'
   })
 });
 
 export const setNewPasswordSchema = z
   .object({
-    password: z.string().min(6, {
+    password: z.string({ required_error: 'Password is required' }).min(6, {
       message: 'Password must be at least 6 characters.'
     }),
-    confirmPassword: z.string().min(6, {
+    confirmPassword: z.string({ required_error: 'Confirm Password is required' }).min(6, {
       message: 'Password must be at least 6 characters.'
     })
   })
