5b673ee6dacfca19e7edc66623ba20b637ddb382
Erwin Mareto Wikas <erwinmaretow@gmail.com>
2024-09-05 22:14:48 +0700

    feat: implemented data processing for spendings chart

--- src/components/parts/SpendingsChart/index.tsx
+++ src/components/parts/SpendingsChart/index.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import { useEffect } from 'react';
-
+import { getMonth } from 'date-fns';
 import { Area, AreaChart, CartesianGrid, XAxis } from 'recharts';
 
 import { Transaction } from '@/components/parts/SubscriptionTable/types';
@@ -13,6 +12,7 @@ import {
   ChartTooltip,
   ChartTooltipContent
 } from '@/components/ui/chart';
+import { MONTHS } from '@/lib/constants/datas';
 
 export interface SpendingsChartProps {
   data?: Transaction[];
@@ -53,50 +53,40 @@ const SpendingsChart = ({
     return total;
   }, 0);
 
-  // this can be deleted i just want need to use the variables for now, the functions can be called outside of useEffect
-  useEffect(() => {
-    console.log(selectedYearData, 'yearrrrrrrrr');
-    console.log(prevYearData, 'prevYearData');
-    console.log(selectedYearTotal, 'totalllllllllll');
-    console.log(prevYearTotal, 'prevYearTotal');
-    console.log(!!selectedYearTotalHandler, '!!!');
-    console.log(!!prevYearTotalHandler);
-    selectedYearTotalHandler && selectedYearTotalHandler(selectedYearTotal ?? 2);
-    prevYearTotalHandler && prevYearTotalHandler(prevYearTotal ?? 45);
-  }, [
-    selectedYearData,
-    prevYearData,
-    selectedYearTotal,
-    prevYearTotal,
-    selectedYearTotalHandler,
-    prevYearTotalHandler
-  ]);
+  const chartData = MONTHS.map((month, monthIndex) => {
+    const year1 = selectedYearData?.reduce((total, transaction) => {
+      if (getMonth(transaction.paymentDate) === monthIndex) {
+        return total + transaction.pricing;
+      }
+      return total;
+    }, 0);
+    const year2 = prevYearData?.reduce((total, transaction) => {
+      if (getMonth(transaction.paymentDate) === monthIndex) {
+        return total + transaction.pricing;
+      }
+      return total;
+    }, 0);
 
-  const chartData = [
-    { month: 'January', 2024: 145, 2023: 282 },
-    { month: 'February', 2024: 312, 2023: 197 },
-    { month: 'March', 2024: 158, 2023: 95 },
-    { month: 'April', 2024: 92, 2023: 264 },
-    { month: 'May', 2024: 218, 2023: 150 },
-    { month: 'June', 2024: 270, 2023: 132 },
-    { month: 'July', 2024: 199, 2023: 348 },
-    { month: 'August', 2024: 256, 2023: 214 },
-    { month: 'September', 2024: 137, 2023: 85 },
-    { month: 'October', 2024: 188, 2023: 299 },
-    { month: 'November', 2024: 212, 2023: 175 },
-    { month: 'December', 2024: 245, 2023: 193 }
-  ];
+    return {
+      month,
+      year1,
+      year2
+    };
+  });
 
   const chartConfig = {
-    2024: {
-      label: 2024,
+    year1: {
+      label: selectedYear,
       color: '#4336f3'
     },
-    2023: {
-      label: 2023,
+    year2: {
+      label: (selectedYear as number) - 1,
       color: '#ecebff'
     }
   } satisfies ChartConfig;
+
+  selectedYearTotalHandler && selectedYearTotalHandler(selectedYearTotal ?? 2);
+  prevYearTotalHandler && prevYearTotalHandler(prevYearTotal ?? 45);
 
   return (
     <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
@@ -108,22 +98,23 @@ const SpendingsChart = ({
           tickMargin={10}
           axisLine={false}
           tickFormatter={(value) => value.slice(0, 3)}
+          interval="preserveStartEnd"
         />
         <ChartTooltip content={<ChartTooltipContent indicator="dot" />} />
         <defs>
           <linearGradient id="secondary-400-gradient" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="var(--color-2024)" stopOpacity={0.8} />
-            <stop offset="95%" stopColor="var(--color-2023)" stopOpacity={0.1} />
+            <stop offset="5%" stopColor="var(--color-year1)" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="var(--color-year2)" stopOpacity={0.1} />
           </linearGradient>
           <linearGradient id="secondary-0-gradient" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="var(--color-2023)" stopOpacity={0.2} />
-            <stop offset="95%" stopColor="var(--color-2023)" stopOpacity={0.1} />
+            <stop offset="5%" stopColor="var(--color-year2)" stopOpacity={0.2} />
+            <stop offset="95%" stopColor="var(--color-year2)" stopOpacity={0.1} />
           </linearGradient>
         </defs>
         <ChartLegend content={<ChartLegendContent />} />
 
-        <Area dataKey="2023" fill="url(#secondary-0-gradient)" strokeWidth={3} stroke="var(--color-2023)" />
-        <Area dataKey="2024" fill="url(#secondary-400-gradient)" strokeWidth={3} stroke="var(--color-2024)" />
+        <Area dataKey="year2" fill="url(#secondary-0-gradient)" strokeWidth={3} stroke="var(--color-year2)" />
+        <Area dataKey="year1" fill="url(#secondary-400-gradient)" strokeWidth={3} stroke="var(--color-year1)" />
       </AreaChart>
     </ChartContainer>
   );
