0443bc007d97e1bede3558dabcefde8d5825efe6
Erwin Mareto Wikas <erwinmaretow@gmail.com>
2024-09-10 23:09:31 +0700

    feat: added query keys for spendings and cost charts data

--- src/lib/constants/queryKeys.ts
+++ src/lib/constants/queryKeys.ts
@@ -7,3 +7,7 @@ export const TRANSACTION_BY_SUB_ID_KEY = 'transaction-by-sub-id';
 export const SUBSCRIPTION_BY_ID = 'subscription-by-id';
 
 export const PROFILE_BY_ID = 'profile-by-id';
+
+export const SPENDINGS_CHART_KEY = 'spending-chart';
+
+export const COST_CHART_KEY = 'cost-chart';
