7e8e4e24534944dd6e237877c54eaacd1583f37d
Erwin Mareto Wikas <erwinmaretow@gmail.com>
2024-10-03 21:53:57 +0700

    refactor: got rid of notifications on navbar

--- src/components/parts/Navbar/RightContent.tsx
+++ src/components/parts/Navbar/RightContent.tsx
@@ -1,14 +1,9 @@
-'use client';
-
-import { useState } from 'react';
-
 import { deleteCookie } from 'cookies-next';
-import { ArrowUpRightIcon, Bell, ChevronDown, X } from 'lucide-react';
+import { ChevronDown } from 'lucide-react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import Identicon from 'react-identicons';
 
-import NotificationCard from '@/components/parts/Navbar/NotificationCard';
 import { Avatar, AvatarFallback } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
 import {
@@ -18,15 +13,11 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu';
-import { Separator } from '@/components/ui/separator';
 import { ACCESS_TOKEN_KEY } from '@/lib/constants/storageKeys';
 import { getCookie } from '@/lib/cookies';
 
 const RightContent = () => {
   const router = useRouter();
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleIsOpen = () => setIsOpen(!isOpen);
 
   const logout = () => {
     deleteCookie(ACCESS_TOKEN_KEY as string);
@@ -36,42 +27,6 @@ const RightContent = () => {
 
   return (
     <div className="hidden justify-center items-center gap-5 lg:flex">
-      <DropdownMenu open={isOpen} onOpenChange={handleIsOpen}>
-        <DropdownMenuTrigger>
-          <Bell />
-        </DropdownMenuTrigger>
-
-        <DropdownMenuContent className="w-[21rem] flex flex-col mt-3 lg:mr-[8rem]">
-          <div className="flex justify-between py-3 mx-3">
-            <p className="font-medium text-body-md text-primary-70">Notifications</p>
-            <X onClick={handleIsOpen} className="cursor-pointer" />
-          </div>
-
-          <DropdownMenuItem>
-            <NotificationCard />
-          </DropdownMenuItem>
-
-          <DropdownMenuSeparator className="mx-3" />
-
-          <DropdownMenuItem>
-            <NotificationCard />
-          </DropdownMenuItem>
-
-          <DropdownMenuSeparator className="mx-3" />
-
-          <DropdownMenuItem>
-            <NotificationCard />
-          </DropdownMenuItem>
-
-          <Link href="/notifications" className="link mt-2 mb-3 ">
-            See all notifications
-            <ArrowUpRightIcon className="w-4 h-4" />
-          </Link>
-        </DropdownMenuContent>
-      </DropdownMenu>
-
-      <Separator orientation="vertical" />
-
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" className="flex justify-center items-center gap-3 hover:bg-transparent p-0">
@@ -86,9 +41,6 @@ const RightContent = () => {
         <DropdownMenuContent className="w-44 flex flex-col p-1 mr-8">
           <Link href="/profile">
             <DropdownMenuItem>My Profile</DropdownMenuItem>
-          </Link>
-          <Link href="/general">
-            <DropdownMenuItem>General Settings</DropdownMenuItem>
           </Link>
           <DropdownMenuSeparator className="my-1" />
           <DropdownMenuItem
